4f6d8127e355b7efdcf7a1062bda1db1cdaf2f7b
HaciyevEli <haciyev.eli457@gmail.com>
2024-08-28 22:19:16 +0400

    The layout is fully rendered

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,44 @@ ScrollReveal().reveal(".header__btns", {
   ...scrollRevealOption,
   delay: 1500,
 });
+
+ScrollReveal().reveal(".description__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".showcase__image img", {
+  ...scrollRevealOption,
+  origin: 'right',
+});
+
+ScrollReveal().reveal(".showcase__content p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".showcase__content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".showcase__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".banner__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".discover__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const mySwiper = new Swiper('.client__swiper', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+})
